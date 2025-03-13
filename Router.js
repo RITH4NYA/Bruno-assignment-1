9668ff69ee3689ce38778203e91e346217c42567
Rithanya <rithanyasasikumar.18@gmail.com>
2025-03-13 12:22:45 +0530

    Router.js

--- Router.js
+++ Router.js
@@ -3,13 +3,19 @@ const {bcrypt} = require ('bcrypt');
 const router = express.Router();
 
 router.post('/signup',async(req,res)=>{
-    const {email,password,name} = req.body
-    const useremail = await user.findOne{email:email};
+    const {name,email,password} = req.body
+    const useremail = await users.findOne{email:email};
     if (usermail){
         return res(200).json({message:"user already exist"});
     }
     const salt = await bcrypt.gensalt(10);
     const pass = await bcrypt.hash(password,salt);
-    const user =({email:email,password: password,name: name});
-     users.create(user);
+    const user = new users({
+        name:name,
+        email:email,
+        password:password
+    })
+    await user.save();
+    
 })
+module.exports = Router;
